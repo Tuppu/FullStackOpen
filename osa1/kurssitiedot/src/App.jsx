ca89e1640f9887ca6d06870949e08b7aaae0694a
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-09 12:27:25 +0300

    1.3: kurssitiedot step3

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -23,20 +23,26 @@ const Part = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[{"part": part1, "exercise": 10},
-         {"part": part2, "exercise": 7},
-         {"part": part3, "exercise": 14}]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Content parts={[{"part": part1.name, "exercise": part1.exercises},
+         {"part": part2.name, "exercise": part2.exercises},
+         {"part": part3.name, "exercise": part3.exercises}]} />
+      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 }
