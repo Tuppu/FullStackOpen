06a6764b371afb9a24bc950b79003b99fae19d2e
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-10 12:52:37 +0300

    2.1: kurssitiedot step6

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,67 @@
+const Course = ({course}) => (
+  <>
+    <Header courseName={course.name} />
+    <Content parts={course.parts} />
+    {/*<Total parts={course.parts} /> */}
+  </>
+);
+
+
+const Header = ({courseName}) => (
+  <h1>{courseName}</h1>
+);
+
+/*
+const Total = ({parts}) => {
+
+  const totaExcercises = parts.reduce((total, current) => {
+        return total + current.exercises;
+    }, 0);
+  
+  return (
+  <p>Number of exercises {totaExcercises}</p>
+)};
+*/
+const Content = ({parts}) => {
+  return (
+  <>
+    {parts.map((i) => {
+      return <Part key={i.id} name={i.name} exercise={i.exercises} />;
+    })}
+  </>
+  );
+};
+
+const Part = ({name, exercise}) => (
+  <p>{name} {exercise}</p>
+);
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+export default App
