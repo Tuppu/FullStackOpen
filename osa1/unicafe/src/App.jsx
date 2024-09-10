5ce748c9b9645d29ca5aee6be5c95bfd0ea16726
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-10 08:23:17 +0300

    1.6: unicafe step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good'>
+        
+      </Button >
+      <Button handleClick={handleNeutralClick} text='neutral'>
+        
+      </Button >
+      <Button handleClick={handleBadClick} text='bad'>
+        
+      </Button >
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
