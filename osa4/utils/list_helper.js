278513b03b6261cb480ecc277fda7e4bcfc450db
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-18 10:17:59 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return blogs.length ?? 0
 }
 
+const totalLikes = (blogs) => {
+  if (!blogs.length) return 0
+  return blogs.reduce((total, { likes }) => total + likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
