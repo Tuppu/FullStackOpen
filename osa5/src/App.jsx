1d25fe767e28dfe1c4680238411a30a6ad9f2d63
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-25 10:47:20 +0300

    5.10: blogilistan frontend, step10

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -19,8 +19,11 @@ const App = () => {
   const blogFormRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+    blogService.getAll().then(blogs => {
+      const orderedBlogs = blogs.sort((a, b) => parseInt(b.likes) - parseInt(a.likes))
+      
+      setBlogs( orderedBlogs )
+    }
     )  
   }, [])
 
