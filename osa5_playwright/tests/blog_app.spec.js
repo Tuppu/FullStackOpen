cc3836c4b2fea440c18d86e5d1f0be13f353f089
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-27 11:39:20 +0300

    5.18: blogilistan end to end ‑testit, step2

--- osa5_playwright/tests/blog_app.spec.js
+++ osa5_playwright/tests/blog_app.spec.js
@@ -14,9 +14,29 @@ describe('Blog app', () => {
         await page.goto('http://localhost:5173')
       })
 
-    test('login page can be opened', async ({ page }) => {
+    test('Login form is shown', async ({ page }) => {
         const locator = await page.getByText('Blogs')
         await expect(locator).toBeVisible()
         await expect(page.getByText('Blog app, FullStackOpen, Tuomas Liikala 2024')).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByRole('button', { name: 'log in' }).click()
+            await page.getByTestId('username').fill('tuppu')
+            await page.getByTestId('password').fill('salsasalsa')
+            await page.getByRole('button', { name: 'login' }).click()
+        
+            await expect(page.getByText('Tuomas Liikala logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByRole('button', { name: 'log in' }).click()
+            await page.getByTestId('username').fill('wrongUser')
+            await page.getByTestId('password').fill('wrongPw')
+            await page.getByRole('button', { name: 'login' }).click()
+        
+            await expect(page.getByText('invalid username or password')).toBeVisible()
+        })
     })
 })
