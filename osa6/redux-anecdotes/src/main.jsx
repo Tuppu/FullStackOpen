26675242db924684a561fb9eb9b0d5455798b464
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-10-03 08:51:17 +0300

    6.14 anekdootit ja backend, step1

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -3,8 +3,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
+)
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
 )
