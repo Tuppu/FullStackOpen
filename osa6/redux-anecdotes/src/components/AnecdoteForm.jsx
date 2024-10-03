480be73e1f81869815202e0612c222e8504e56c0
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-10-03 08:59:46 +0300

    6.15 anekdootit ja backend, step2

--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,19 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification, hideNotification } from '../reducers/notificationReducer'
 import { useDispatch } from 'react-redux'
+import anecdoteService from '../services/anecdotes'
 
 const AnecodoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.content.value
         event.target.content.value = ''
-        dispatch(createAnecdote(content))
+
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
+
         dispatch(showNotification((`${content} added`)))
         setTimeout(() => {
             dispatch(hideNotification())
