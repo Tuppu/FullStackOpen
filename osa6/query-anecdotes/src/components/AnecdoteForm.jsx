dc3b7be1c0e9417aeb2353babc4740480dab8917
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-10-04 13:17:06 +0300

    Tehtävä 6.24.

--- osa6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -6,22 +6,38 @@ import NotificationContext from '../NotificationContext'
 const AnecdoteForm = () => {
   const [notification, dispatch] = useContext(NotificationContext)
   const queryClient = useQueryClient()
-  const newAnecodteMutation = useMutation({
-    mutationFn: createAnecdote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
-    },
-  })
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    newAnecodteMutation.mutate({ content, votes: 0 })
+  const successNotification = (content) => {
     dispatch({ type: "SHOW", message: `anecdote '${content}' created`})
     setTimeout(() => {
       dispatch({ type: "HIDE"})
     }, 5000)
+  }
+
+  const errorNotification = (error) => {
+    dispatch({ type: "SHOW", message: error.response.data.error})
+    setTimeout(() => {
+      dispatch({ type: "HIDE"})
+    }, 5000)
+  }
+
+  const newAnecodteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (notifcation) => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      successNotification(notifcation.content)
+    },
+    onError: (error) => {
+      errorNotification(error)
+    }
+  })
+
+  const onCreate = (event) => {
+
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecodteMutation.mutate({ content, votes: '' })
 }
 
   return (
--- osa6/query-anecdotes/src/components/Notification.jsx
+++ osa6/query-anecdotes/src/components/Notification.jsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react'
 import { UseNotificationValue }  from '../NotificationContext'
 
 const Notification = () => {
