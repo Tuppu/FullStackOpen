c4652e4170e7969c01c2d4d7939bad0184c327c7
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-10-28 09:12:22 +0200

    8.19 Genren kirjat

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,28 @@
-const Books = (props) => {
-  if (!props.show) {
+import { useState } from "react"
+
+const Books = ({ allBooks, show }) => {
+  const [genreState, setGenre] = useState("")
+
+  if (!show) {
     return null
   }
 
-  const books = props.allBooks
+  let genres = []
+  for (const book of allBooks) {
+    genres = genres.concat(book.genres)
+  }
+
+  let books = allBooks
+  if (genreState) {
+    books = allBooks.filter((book) => {
+      const found = book.genres.includes(genreState)
+      if (found) return book
+    })
+  }
+
+  const addGenre = (genre) => {
+    setGenre(genre?.target?.value ?? "")
+  }
 
   return (
     <div>
@@ -25,6 +44,14 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {genres.map((genre) => (
+        <button key={genre} value={genre} onClick={(genre) => addGenre(genre)} type='button'>
+          {genre}
+        </button>
+      ))}
+      <button value='' onClick={() => addGenre()} type='button'>
+        all genres
+      </button>
     </div>
   )
 }
