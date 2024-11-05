d18c90d29f73b08f861dea76264081e83e170c63
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-11-05 07:30:22 +0200

    9.9: Patientor backend, step2

---
+++ osa9/patientor-backend/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PORT = 3001;
+app.get('/api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.json('{"content": "Pong!"}');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+
 const app = express();
+
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
-  res.send('pong');
+  res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.json('{"content": "Pong!"}');
 });
 
 app.listen(PORT, () => {
