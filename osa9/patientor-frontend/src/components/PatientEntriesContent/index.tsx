0bb1ecf4e9d4830d47caebc481dda6c30b3a60eb
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-11-15 14:01:09 +0200

    9.25: Patientor, step 5

--- osa9/patientor-frontend/src/components/PatientEntriesContent/index.tsx
+++ osa9/patientor-frontend/src/components/PatientEntriesContent/index.tsx
@@ -1,16 +1,28 @@
 import { Typography } from '@mui/material';
-import { Entry } from '../../types';
+import { Diagnose, Entry } from '../../types';
+import { useEffect, useState } from 'react';
+import diagnosesService from "../../services/diagnoses";
 
 interface HeaderProps {
   entries: Entry[] | undefined;
 }
 
 const PatientEntriesContent = ({entries}: HeaderProps): JSX.Element => {
+  
+  const [diagnoses, setDiagnoses] = useState<Diagnose[]>([]);
+  
+  useEffect(() => {
+    const fetchPatientList = async () => {
+      const diagnoses = await diagnosesService.getAll();
+      setDiagnoses(diagnoses);
+    };
+    void fetchPatientList();
+  }, []);
+
   if (entries === undefined || entries.length === 0) {
     return <></>;
   }
   
-
   return (
     <div>
       <Typography variant="h6" style={{ marginTop: "0.5em", marginBottom: "0.5em" }}>
@@ -20,9 +32,12 @@ const PatientEntriesContent = ({entries}: HeaderProps): JSX.Element => {
       <Typography variant="body1" gutterBottom>
       {entry.date} {entry.description}
       <ul>
-      {entry.diagnosisCodes?.map((diagnose) => (
-          <li>{diagnose}</li>
-      ))}
+        {entry.diagnosisCodes?.map((diagnose) => {
+          const diagnoseDetails = diagnoses.find((d => d.code == diagnose));
+          return (
+            <li>{diagnoseDetails?.code} {diagnoseDetails?.name}</li>
+          );}
+        )}
       </ul>
       </Typography>
       ))}
